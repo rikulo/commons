6254d243f28204689db219a8467633356c238589
Henri Chen <henrichen@zkoss.org>
2012-10-23 11:30:39 +0800

    Utility for js-interop.

--- lib/html.dart
+++ lib/html.dart
@@ -6,9 +6,11 @@ library rikulo_html;
 
 import "dart:html";
 import "dart:math";
+import "package:js/js.dart" as js;
 
 import "util.dart";
 
 part "src/html/Browser.dart";
 part "src/html/CSS.dart";
 part "src/html/DOMAgent.dart";
+part "src/html/JSUtil.dart";
