172b9574b22a5d1c2e331a8b962042e0d5ee67d3
tomyeh <tomyeh@potix.com>
2013-04-09 19:37:16 +0800

    add test/run.sh

---
+++ test/run_all.dart
@@ -0,0 +1,20 @@
+//Copyright (C) 2013 Potix Corporation. All Rights Reserved.
+//History: Tue, Apr 09, 2013  4:50:32 PM
+// Author: tomyeh
+library test_run_all;
+
+import 'dart:io' show Options;
+import 'package:unittest/compact_vm_config.dart';
+import 'package:unittest/unittest.dart';
+
+import 'inject_test.dart' as inject_test;
+import 'io_test.dart' as io_test;
+import 'util_test.dart' as util_test;
+
+main() {
+  useCompactVMConfiguration();
+
+  io_test.main();
+  inject_test.main();
+  util_test.main();
+}
