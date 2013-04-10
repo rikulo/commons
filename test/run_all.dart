6b85bf648e433e3f5822f20306ef80c767a5ab5e
tomyeh <tomyeh@potix.com>
2013-04-10 10:54:45 +0800

    minor

--- test/run_all.dart
+++ test/run_all.dart
@@ -4,6 +4,7 @@
 library test_run_all;
 
 import 'dart:io' show Options;
+import 'package:unittest/compact_vm_config.dart';
 import 'package:unittest/unittest.dart';
 
 import 'inject_test.dart' as inject_test;
@@ -11,6 +12,8 @@ import 'io_test.dart' as io_test;
 import 'util_test.dart' as util_test;
 
 main() {
+  useCompactVMConfiguration();
+
   io_test.main();
   inject_test.main();
   util_test.main();
