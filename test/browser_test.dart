7b5cb4b5c33906523bfe0a49294e18c91e53c43e
Tom Yeh <tomyeh@potix.com>
2026-02-24 12:32:42 +0800

    test case

--- test/browser_test.dart
+++ test/browser_test.dart
@@ -13,138 +13,139 @@ main() {
   });
 
   void isChrome(Browser browser, num version) {
-    expect(browser.chrome, isTrue);
-    expect(browser.webkit, isTrue);
-    expect(browser.firefox, isFalse);
+    expect(browser.chrome, true);
+    expect(browser.webkit, true);
+    expect(browser.firefox, false);
+    expect(browser.safari, false);
     expect(browser.version, version);
   }
   void isLegacyEdge(Browser browser, num version) {
-    expect(browser.webkit, isTrue);
-    expect(browser.legacyEdge, isTrue);
-    expect(browser.newEdge, isFalse);
-    expect(browser.ie, isFalse); //Edge is not IE
-    //expect(browser.chrome, isFalse);
-    expect(browser.firefox, isFalse);
+    expect(browser.webkit, true);
+    expect(browser.legacyEdge, true);
+    expect(browser.newEdge, false);
+    expect(browser.ie, false); //Edge is not IE
+    //expect(browser.chrome, false);
+    expect(browser.firefox, false);
     //expect(browser.version, version); //not correct; don't check
   }
   void isNewEdge(Browser browser, num version) {
-    expect(browser.webkit, isTrue);
-    expect(browser.newEdge, isTrue);
-    expect(browser.legacyEdge, isFalse);
-    expect(browser.ie, isFalse); //Edge is not IE
-    expect(browser.chrome, isTrue); //New Edge is also Chrome
-    expect(browser.firefox, isFalse);
+    expect(browser.webkit, true);
+    expect(browser.newEdge, true);
+    expect(browser.legacyEdge, false);
+    expect(browser.ie, false); //Edge is not IE
+    expect(browser.chrome, true); //New Edge is also Chrome
+    expect(browser.firefox, false);
     expect(browser.version, version);
   }
   void isIE(Browser browser, num version) {
-    expect(browser.ie, isTrue);
-    expect(browser.legacyEdge, isFalse);
-    expect(browser.webkit, isFalse);
-    expect(browser.chrome, isFalse);
-    expect(browser.firefox, isFalse);
+    expect(browser.ie, true);
+    expect(browser.legacyEdge, false);
+    expect(browser.webkit, false);
+    expect(browser.chrome, false);
+    expect(browser.firefox, false);
     expect(browser.version, version);
   }
   void isSafari(Browser browser, num version) {
-    expect(browser.safari, isTrue);
-    expect(browser.webkit, isTrue);
-    expect(browser.chrome, isFalse);
-    expect(browser.firefox, isFalse);
+    expect(browser.safari, true);
+    expect(browser.webkit, true);
+    expect(browser.chrome, false);
+    expect(browser.firefox, false);
     expect(browser.version, version);
   }
   void isFirefox(Browser browser, num version) {
-    expect(browser.safari, isFalse);
-    expect(browser.webkit, isFalse);
-    expect(browser.chrome, isFalse);
-    expect(browser.firefox, isTrue);
+    expect(browser.safari, false);
+    expect(browser.webkit, false);
+    expect(browser.chrome, false);
+    expect(browser.firefox, true);
     expect(browser.version, version);
   }
   void isOpera(Browser browser, num version) {
-    expect(browser.safari, isFalse);
-    expect(browser.webkit, isFalse);
-    expect(browser.ie, isFalse);
-    expect(browser.opera, isTrue);
+    expect(browser.safari, false);
+    expect(browser.webkit, false);
+    expect(browser.ie, false);
+    expect(browser.opera, true);
     expect(browser.version, version);
   }
   void isWebkit(Browser browser, num version) {
-    expect(browser.safari, isFalse);
-    expect(browser.webkit, isTrue);
+    expect(browser.safari, false);
+    expect(browser.webkit, true);
     expect(browser.version, version);
   }
 
   group("browser tests", () {
     test("browser tests", () {
-      final Map<String, List> uas = {
-"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36 Chrome 41.0.2227.1":
-  [isChrome, 41.0],
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36 Chrome 41.0.2227.0":
-  [isChrome, 41.0],
-"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36":
-  [isChrome, 41.0],
-"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36":
-  [isChrome, 41.0],
+      final uas = [
+("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36 Chrome 41.0.2227.1",
+  isChrome, 41.0),
+("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36 Chrome 41.0.2227.0",
+  isChrome, 41.0),
+("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+  isChrome, 41.0),
+("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+  isChrome, 41.0),
 
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A":
-  [isSafari, 7.0],
-"Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25":
-  [isSafari, 6.0],
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15":
-  [isSafari, 16.3],
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3 Safari/605.1.15":
-  [isSafari, 18.3],
+("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A",
+  isSafari, 7.0),
+("Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25",
+  isSafari, 6.0),
+("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15",
+  isSafari, 16.3),
+("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3 Safari/605.1.15",
+  isSafari, 18.3),
 
-"Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko":
-  [isIE, 11.0],
-"Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko":
-  [isIE, 11.0],
-"Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0":
-  [isIE, 10.6],
+("Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+  isIE, 11.0),
+("Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko",
+  isIE, 11.0),
+("Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0",
+  isIE, 10.6),
 
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246":
-  [isLegacyEdge, 12.246],
+("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+  isLegacyEdge, 12.246),
 
-"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1":
-  [isFirefox, 40.1],
-"Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0":
-  [isFirefox, 36.0],
+("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
+  isFirefox, 40.1),
+("Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+  isFirefox, 36.0),
 
-"Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16":
-  [isOpera, 12.16],
-"Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14":
-  [isOpera, 12.14],
-"Mozilla/5.0 (Windows NT 6.0; rv:2.0) Gecko/20100101 Firefox/4.0 Opera 12.14":
-  [isOpera, 12.14],
-"Xiaomi_MDT2_TD-LTE/V1 Linux/3.18.31 Android/7.1 Release/5.15.2017 "
-"Browser/AppleWebKit537.36 Mobile Safari/537.36 System/Android 7.1 XiaoMi/MiuiBrowser/8.7.7":
-  [isWebkit, 537.36],
+("Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
+  isOpera, 12.16),
+("Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14",
+  isOpera, 12.14),
+("Mozilla/5.0 (Windows NT 6.0; rv:2.0) Gecko/20100101 Firefox/4.0 Opera 12.14",
+  isOpera, 12.14),
+("Xiaomi_MDT2_TD-LTE/V1 Linux/3.18.31 Android/7.1 Release/5.15.2017 "
+"Browser/AppleWebKit537.36 Mobile Safari/537.36 System/Android 7.1 XiaoMi/MiuiBrowser/8.7.7",
+  isWebkit, 537.36),
+
+//Chrome on iOS
+(criOS, isChrome, 134.0),
 
 //New Edge
-newEdge:
-  [isNewEdge, 83.0],
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.74 Safari/537.36 Edg/79.0.309.43":
-  [isNewEdge, 79.0],
-newEdgeAndroid:
-  [isNewEdge, 136.0],
-      };
+(newEdge, isNewEdge, 83.0),
+("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.74 Safari/537.36 Edg/79.0.309.43",
+  isNewEdge, 79.0),
+(newEdgeAndroid, isNewEdge, 136.0),
+      ];
 
-      for (final ua in uas.keys) {
+      for (final (ua, fn, version) in uas) {
 //        print(ua);
         final browser = _Browser(ua);
-        final result = uas[ua]!;
-        result[0](browser, result[1]);
+        fn(browser, version);
       }
 
       var browser = _Browser(newEdge);
-      expect(browser.legacyEdge, isFalse);
-      expect(browser.webkit, isTrue);
+      expect(browser.legacyEdge, false);
+      expect(browser.webkit, true);
 
       browser = _Browser(newEdgeAndroid);
-      expect(browser.mobile, isTrue);
+      expect(browser.mobile, true);
 
       browser = _Browser(edgeIOS);
-      expect(browser.mobile, isTrue);
-      expect(browser.chrome, isFalse);
-      expect(browser.iOS, isTrue);
-      expect(browser.newEdge, isFalse); //newEdge limited to Chrome based only (for JS compatibility)
+      expect(browser.mobile, true);
+      expect(browser.chrome, false);
+      expect(browser.iOS, true);
+      expect(browser.newEdge, false); //newEdge limited to Chrome based only (for JS compatibility)
     });
 
     test("OS test", () {
@@ -152,7 +153,7 @@ newEdgeAndroid:
 "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-en) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4",
 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15",
 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.78.2 (KHTML, like Gecko) Version/7.0.6 Safari/537.78.2"]) {
-        expect(_Browser(ua).macOS, isTrue);
+        expect(_Browser(ua).macOS, true);
       }
 
       for (final ua in [
@@ -161,7 +162,7 @@ newEdgeAndroid:
 "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/74.0.3729.157 Safari/537.36",
 "Opera/9.80 (Linux armv7l) Presto/2.12.407 Version/12.51 , D50u-D1-UHD/V1.5.16-UHD (Vizio, D50u-D1, Wireless)"]) {
         //print(ua);
-        expect(_Browser(ua).linux, isTrue);
+        expect(_Browser(ua).linux, true);
       }
 
       for (final ua in [
@@ -170,13 +171,20 @@ newEdgeAndroid:
 "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36",
 "Opera/9.80 (Windows Mobile; Opera Mini/5.1.21594/28.1977; U; en) Presto/2.8.119 Version/11.10"]) {
         //print(ua);
-        expect(_Browser(ua).windows, isTrue);
+        expect(_Browser(ua).windows, true);
       }
+
+      final bw = _Browser(criOS);
+      expect(bw.iOS, true);
+      expect(bw.iOSVersion, 16.3);
     });
   });
 }
 
-const newEdge =
+const
+  criOS =
+  "Mozilla/5.0 (iPad; CPU OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/134.0.6998.33 Mobile/15E148 Safari/604.1",
+  newEdge =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
   "Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.45",
   newEdgeAndroid =
